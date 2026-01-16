a4cb67f9b15763a54857b1169e2703c0aeec29d9
charan-0310 <charansundar.moorthy@kumaran.com>
2026-01-16 15:43:57 +0530

    Fix Error

--- src/main/java/com/vimainsurance/vimaadmin/service/serviceimpl/PolicyServiceImpl.java
+++ src/main/java/com/vimainsurance/vimaadmin/service/serviceimpl/PolicyServiceImpl.java
@@ -91,8 +91,6 @@ public class PolicyServiceImpl implements IPolicyService {
     @Autowired
     private IDocumentService documentService;
 
-    @Autowired
-    private IDealsRepository dealsRepository;
 
     @Override
     public ResponseEntity<ResponseDto<String>> createPolicy(PolicyRequestDto requestDto) {
