408b5849290ed91f47268ddb8b9c75a1daef94e0
Mansel <sanjaymansel.selvan@kumaran.com>
2026-01-21 14:03:05 +0530

    null check fix

--- src/main/java/com/vimainsurance/vimaadmin/service/serviceimpl/AdminUserServiceImpl.java
+++ src/main/java/com/vimainsurance/vimaadmin/service/serviceimpl/AdminUserServiceImpl.java
@@ -100,7 +100,7 @@ public class AdminUserServiceImpl implements IAdminUserService {
             if(requestDto.getRole().contains("HR_ADMIN") && (requestDto.getOrganizations() == null || requestDto.getOrganizations().isEmpty())) {
                 return responseObj.render(responseObj.formErrorResponse("Organizations are required"));
             }
-            if(requestDto.getRole().contains("HR_ADMIN") && requestDto.getOrganizations().size() > 1) {
+            if(requestDto.getRole().contains("HR_ADMIN") && requestDto.getOrganizations() != null && requestDto.getOrganizations().size() > 1) {
                 return responseObj.render(responseObj.formErrorResponse("HR_ADMIN can only be assigned to one organization"));
             }
             // Create user in Authentik first
